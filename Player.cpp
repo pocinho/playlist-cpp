b8f25e0ae6865bbeb0834369a8dba73133a038b4
pocinho <pocinho@gmail.com>
2017-03-13 01:57:34 +0000

    Development checkpoint
    Development checkpoint

--- Player.cpp
+++ Player.cpp
@@ -36,11 +36,11 @@ void Player::InicializarDB()
 	{
 		if (!db_.tableExists("playlists"))
 		{
-			db_.exec("CREATE TABLE playlists (id INTEGER PRIMARY KEY, nome CHAR(50))");
+			db_.exec("CREATE TABLE playlists (id INTEGER PRIMARY KEY, nome CHAR(50), posicao INTEGER)");
 		}
 		else
 		{
-			SQLite::Statement query(db_, "SELECT nome FROM playlists");
+			SQLite::Statement query(db_, "SELECT nome FROM playlists ORDER BY posicao");
 			while (query.executeStep())
 			{
 				const std::string nome = query.getColumn(0);
@@ -50,11 +50,11 @@ void Player::InicializarDB()
 
 		if (!db_.tableExists("musicas"))
 		{
-			db_.exec("CREATE TABLE musicas (id INTEGER PRIMARY KEY, titulo CHAR(50), autor CHAR(50), estilo CHAR(50), ano INTEGER, duracao REAL, ficheiro CHAR(200), playlist INTEGER)");
+			db_.exec("CREATE TABLE musicas (id INTEGER PRIMARY KEY, titulo CHAR(50), autor CHAR(50), estilo CHAR(50), ano INTEGER, duracao REAL, ficheiro CHAR(200), playlist INTEGER, posicao INTEGER)");
 		}
 		else
 		{
-			SQLite::Statement query(db_, "SELECT titulo, autor, estilo, ano, duracao, ficheiro, playlist FROM musicas");
+			SQLite::Statement query(db_, "SELECT titulo, autor, estilo, ano, duracao, ficheiro, playlist FROM musicas ORDER BY posicao");
 			while (query.executeStep())
 			{
 				const std::string titulo   = query.getColumn(0);
@@ -64,7 +64,7 @@ void Player::InicializarDB()
 				const double      duracao  = query.getColumn(4);
 				const std::string ficheiro = query.getColumn(5);
 				const int         playlist = query.getColumn(6);
-				lista_[playlist].Adicionar(Musica(titulo, autor, estilo, ano, duracao, ficheiro));
+				lista_[playlist - 1].Adicionar(Musica(titulo, autor, estilo, ano, duracao, ficheiro));
 			}
 		}
 	}
@@ -80,8 +80,10 @@ void Player::AdicionarPlaylist(std::string nome)
 {
 	try
 	{
-		string query("INSERT INTO playlists VALUES (NULL, \"" + nome + "\")");
-		db_.exec(query.c_str());
+		const int posicao = lista_.size() + 1;
+		stringstream query;
+		query << "INSERT INTO playlists VALUES (NULL, \"" << nome << "\", \"" << posicao << "\")";
+		db_.exec(query.str());
 	}
 	catch (std::exception& e)
 	{
@@ -101,22 +103,26 @@ void Player::AdicionarMusica(VecInt playlist, Musica musica)
 	{
 		try
 		{
-			string query = "INSERT INTO musicas VALUES (NULL, \"";
-			query += musica.GetTitulo();
-			query += "\", \"";
-			query += musica.GetAutor();
-			query += "\", \"";
-			query += musica.GetEstilo();
-			query += "\", \"";
-			query += musica.GetAno();
-			query += "\", \"";
-			query += musica.GetDuracao();
-			query += "\", \"";
-			query += musica.GetFicheiro();
-			query += "\", \"";
-			query += playlist;
-			query += "\")";
-			db_.exec(query);
+			int posicao = lista_[pos].GetTotalMusicas() + 1;
+			stringstream query;
+			query << "INSERT INTO musicas VALUES (NULL, \"";
+			query << musica.GetTitulo();
+			query << "\", \"";
+			query << musica.GetAutor();
+			query << "\", \"";
+			query << musica.GetEstilo();
+			query << "\", \"";
+			query << musica.GetAno();
+			query << "\", \"";
+			query << musica.GetDuracao();
+			query << "\", \"";
+			query << musica.GetFicheiro();
+			query << "\", \"";
+			query << playlist;
+			query << "\", \"";
+			query << posicao;
+			query << "\")";
+			db_.exec(query.str().c_str());
 		}
 		catch (std::exception& e)
 		{
@@ -135,25 +141,81 @@ void Player::RemoverPlaylist(VecInt posicao)
 	}
 	else
 	{
+		try
+		{
+			stringstream query1;
+			query1 << "DELETE FROM musicas WHERE playlist = " << posicao;
+			db_.exec(query1.str());
 
+			stringstream query2;
+			query2 << "DELETE FROM playlists WHERE posicao = " << posicao;
+			db_.exec(query2.str());
+		}
+		catch (std::exception& e)
+		{
+			std::cout << "Erro ao eliminar playlist na base de dados: " << e.what() << std::endl;
+		}
 		lista_.erase(lista_.begin() + pos);
 	}
 }
 
-void Player::RemoverMusica(VecInt playlist, VecInt musica)
+void Player::RemoverMusica(VecInt playlist, VecInt posicao)
 {
 	VecInt pos = playlist - 1;
 	if (pos < 0 || pos >= lista_.size())
 	{
 		cout << "Não é possivel encontrar a playlist " << playlist << ".\n";
 	}
-	else if (!lista_[pos].HasObject(musica))
+	else if (!lista_[pos].HasObject(posicao))
 	{
-		cout << "Não é possivel encontrar a musica " << musica << " na playlist " << playlist << ".\n";
+		cout << "Não é possivel encontrar a musica " << posicao << " na playlist " << playlist << ".\n";
 	}
 	else
 	{
-		lista_[playlist].Remover(musica);
+		try
+		{
+			stringstream query;
+			query << "DELETE FROM musicas WHERE playlist = " << playlist;
+			db_.exec(query.str());
+		}
+		catch (std::exception& e)
+		{
+			std::cout << "Erro ao eliminar musica na base de dados: " << e.what() << std::endl;
+		}
+
+		lista_[playlist].Remover(posicao);
+
+		int posicao_musica = 0;
+		for (auto& musica : lista_[playlist].GetLista())
+		{
+			posicao_musica += 1;
+			try
+			{
+				stringstream query;
+				query << "INSERT INTO musicas VALUES (NULL, \"";
+				query << musica.GetTitulo();
+				query << "\", \"";
+				query << musica.GetAutor();
+				query << "\", \"";
+				query << musica.GetEstilo();
+				query << "\", \"";
+				query << musica.GetAno();
+				query << "\", \"";
+				query << musica.GetDuracao();
+				query << "\", \"";
+				query << musica.GetFicheiro();
+				query << "\", \"";
+				query << playlist;
+				query << "\", \"";
+				query << posicao_musica;
+				query << "\")";
+				db_.exec(query.str());
+			}
+			catch (std::exception& e)
+			{
+				std::cout << "Erro ao reordenar musica na base de dados: " << e.what() << std::endl;
+			}
+		}
 	}
 }
 
@@ -175,8 +237,87 @@ void Player::AlocarMusica(VecInt playlist_origem, VecInt musica, VecInt playlist
 	}
 	else
 	{
+		try
+		{
+			stringstream query1;
+			query1 << "DELETE FROM musicas WHERE playlist = " << playlist_origem;
+			db_.exec(query1.str());
+
+			stringstream query2;
+			query2 << "DELETE FROM musicas WHERE playlist = " << playlist_destino;
+			db_.exec(query2.str());
+		}
+		catch (std::exception& e)
+		{
+			std::cout << "Erro ao eliminar musica na base de dados: " << e.what() << std::endl;
+		}
+
 		lista_[pos_destino].Adicionar(lista_[pos_origem].GetMusica(musica));
 		lista_[pos_origem].Remover(musica);
+
+		int posicao_musica = 0;
+		for (auto& musica : lista_[playlist_origem].GetLista())
+		{
+			posicao_musica += 1;
+			try
+			{
+				stringstream query;
+				query << "INSERT INTO musicas VALUES (NULL, \"";
+				query << musica.GetTitulo();
+				query << "\", \"";
+				query << musica.GetAutor();
+				query << "\", \"";
+				query << musica.GetEstilo();
+				query << "\", \"";
+				query << musica.GetAno();
+				query << "\", \"";
+				query << musica.GetDuracao();
+				query << "\", \"";
+				query << musica.GetFicheiro();
+				query << "\", \"";
+				query << playlist_origem;
+				query << "\", \"";
+				query << posicao_musica;
+				query << "\")";
+				db_.exec(query.str());
+			}
+			catch (std::exception& e)
+			{
+				std::cout << "Erro ao reordenar musica na base de dados: " << e.what() << std::endl;
+			}
+		}
+
+		posicao_musica = 0;
+		for (auto& musica : lista_[playlist_destino].GetLista())
+		{
+			posicao_musica += 1;
+			try
+			{
+				stringstream query;
+				query << "INSERT INTO musicas VALUES (NULL, \"";
+				query << musica.GetTitulo();
+				query << "\", \"";
+				query << musica.GetAutor();
+				query << "\", \"";
+				query << musica.GetEstilo();
+				query << "\", \"";
+				query << musica.GetAno();
+				query << "\", \"";
+				query << musica.GetDuracao();
+				query << "\", \"";
+				query << musica.GetFicheiro();
+				query << "\", \"";
+				query << playlist_destino;
+				query << "\", \"";
+				query << posicao_musica;
+				query << "\")";
+				db_.exec(query.str());
+			}
+			catch (std::exception& e)
+			{
+				std::cout << "Erro ao reordenar musica na base de dados: " << e.what() << std::endl;
+			}
+		}
 	}
 }
 
@@ -197,7 +338,50 @@ void Player::ReordenarPlaylist(VecInt playlist, VecInt musica_origem, VecInt mus
 	}
 	else
 	{
+		try
+		{
+			stringstream query;
+			query << "DELETE FROM musicas WHERE playlist = " << playlist;
+			db_.exec(query.str());
+		}
+		catch (std::exception& e)
+		{
+			std::cout << "Erro ao eliminar musica na base de dados: " << e.what() << std::endl;
+		}
+
 		lista_[pos].Reordenar(musica_origem, musica_destino);
+
+		int posicao_musica = 0;
+		for (auto& musica : lista_[playlist].GetLista())
+		{
+			posicao_musica += 1;
+			try
+			{
+				stringstream query;
+				query << "INSERT INTO musicas VALUES (NULL, \"";
+				query << musica.GetTitulo();
+				query << "\", \"";
+				query << musica.GetAutor();
+				query << "\", \"";
+				query << musica.GetEstilo();
+				query << "\", \"";
+				query << musica.GetAno();
+				query << "\", \"";
+				query << musica.GetDuracao();
+				query << "\", \"";
+				query << musica.GetFicheiro();
+				query << "\", \"";
+				query << playlist;
+				query << "\", \"";
+				query << posicao_musica;
+				query << "\")";
+				db_.exec(query.str());
+			}
+			catch (std::exception& e)
+			{
+				std::cout << "Erro ao reordenar musica na base de dados: " << e.what() << std::endl;
+			}
+		}
 	}
 }
 
@@ -274,6 +458,17 @@ void Player::AlterarPlaylist(VecInt playlist, std::string nome)
 	}
 	else
 	{
+		try
+		{
+			stringstream query;
+			query << "UPDATE playlists SET nome = " << nome << " WHERE posicao = " << playlist;
+			db_.exec(query.str());
+		}
+		catch (std::exception& e)
+		{
+			std::cout << "Erro ao renomear playlist na base de dados: " << e.what() << std::endl;
+		}
+
 		lista_[pos].SetNome(nome);
 	}
 }
@@ -291,6 +486,31 @@ void Player::AlterarMusica(VecInt playlist, VecInt posicao, Musica musica)
 	}
 	else
 	{
+		try
+		{
+			stringstream query;
+			query << "UPDATE musicas SET titulo = ";
+			query << musica.GetTitulo();
+			query << ", autor = ";
+			query << musica.GetAutor();
+			query << ", estilo = ";
+			query << musica.GetEstilo();
+			query << ", ano = ";
+			query << musica.GetAno();
+			query << ", duracao = ";
+			query << musica.GetDuracao();
+			query << ", ficheiro = ";
+			query << musica.GetFicheiro();
+			query << " WHERE playlist = ";
+			query << playlist;
+			query << " AND posicao = ";
+			query << posicao;
+			db_.exec(query.str());
+		}
+		catch (std::exception& e)
+		{
+			std::cout << "Erro ao actualizar musica na base de dados: " << e.what() << std::endl;
+		}
 		lista_[pos].AlterarMusica(posicao, musica);
 	}
 }
